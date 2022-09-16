fd8256c032bdf0aae45563be095f3a8a14ca5e92
Adrian McFarlane <confuzedfx@gmail.com>
2022-09-15 23:41:58 -0700

    iam module

--- terraform/modules/iam/iam.tf
+++ terraform/modules/iam/iam.tf
@@ -1,30 +1,30 @@
 resource "aws_iam_role" "access_user" {
-    name = "access_user"
-    assume_role_policy = <<EOF
+  name               = "access_user"
+  assume_role_policy = <<EOF
+{
+"Version": "2012-10-17",
+"Statement": [
     {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-            "Service": "ec2.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-        }
-    ]
+    "Action": "sts:AssumeRole",
+    "Principal": {
+        "Service": "ec2.amazonaws.com"
+    },
+    "Effect": "Allow",
+    "Sid": ""
     }
-    EOF
+]
+}
+EOF
 }
 
 resource "aws_iam_role_policy_attachment" "codecommit" {
-    role = aws_iam_role.access_user.name
-    policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitFullAccess"
+  role       = aws_iam_role.access_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeCommitFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "sns" {
-    role = aws_iam_role.access_user.name
-    policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+  role       = aws_iam_role.access_user.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
 }
 
 
